a181d3ced77f73c0142ca440c9245b552ac7af8d
VeigaCoder <gbrl316@gmail.com>
2021-09-20 20:03:39 -0300

    Rebuilt App and made Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
+import { Home } from './components'
 
 const App = () => {
   return (
-    <div>
-      a
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/components/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>HOME</div>
+  )
+}
+
+export default Home
