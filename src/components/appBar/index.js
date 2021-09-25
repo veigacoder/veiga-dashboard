a3d555a63623f6c6fd33e54195c964ade5121ff7
VeigaCoder <gbrl316@gmail.com>
2021-09-25 00:30:34 -0300

    Imported sun and moon

--- src/components/appBar/index.js
+++ src/components/appBar/index.js
@@ -3,6 +3,8 @@ import { Card, CardBody } from '../../containers/Card'
 import { ContainerRow, PageRow } from '../../containers/Display'
 import dashboard from '../../assets/icons/dashboard.svg'
 import person from '../../assets/icons/person.svg'
+import sun from '../../assets/icons/sun.svg'
+import moon from '../../assets/icons/moon.svg'
 import { BarButton } from '../../containers/Buttons'
 
 export const AppBar = () => {
@@ -10,7 +12,7 @@ export const AppBar = () => {
     <Bar>
       <BarContent>
         <Card round>
-          <ContainerRow>
+          <PageRow number='5' smNumber='5' mdNumber='5'>
             <BarButton>
               <img src={dashboard} alt='dashboard' width='24px' height='24px' />
             </BarButton>
@@ -18,7 +20,16 @@ export const AppBar = () => {
             <BarButton>
               <img src={person} alt='person' width='24px' height='24px' />
             </BarButton>
-          </ContainerRow>
+
+            <BarButton>
+              <img src={sun} alt='sun' width='24px' height='24px' />
+            </BarButton>
+
+            <BarButton>
+              <img src={moon} alt='moon' width='24px' height='24px' />
+            </BarButton>
+          </PageRow>
+
         </Card>
       </BarContent>
     </Bar>
